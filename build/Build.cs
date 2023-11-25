91ceefca67f82e868543ec7570ec33c74e39b1cf
LipatovAlexander <lipatov.work@bk.ru>
2023-11-26 00:05:03 +0300

    feat: Publish to nuget

--- build/Build.cs
+++ build/Build.cs
@@ -2,14 +2,16 @@ using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [GitHubActions("build",
     GitHubActionsImage.UbuntuLatest,
-    OnPushBranches = new[] { "master", "main" },
-    InvokedTargets = new[] { nameof(Clean), nameof(Test) })]
+    OnPushBranches = new[] { "master" },
+    InvokedTargets = new[] { nameof(Clean), nameof(Test), nameof(PushToNuGet) },
+    ImportSecrets = new[] { nameof(NuGetToken) })]
 class Build : NukeBuild
 {
     public static int Main() => Execute<Build>(x => x.Compile);
@@ -21,7 +23,12 @@ class Build : NukeBuild
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
+    [Parameter] readonly string NuGetToken;
+
+    const string NugetOrgUrl = "https://api.nuget.org/v3/index.json";
+    
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
@@ -44,6 +51,7 @@ class Build : NukeBuild
             DotNetBuild(s => s
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
+                .When(IsServerBuild, x => x.SetProperty("ContinuousIntegrationBuild", "true"))
                 .EnableNoRestore());
         });
 
@@ -56,5 +64,35 @@ class Build : NukeBuild
                 .SetConfiguration(Configuration)
                 .EnableNoBuild()
                 .EnableNoRestore());
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .After(Test)
+        .Produces(ArtifactsDirectory)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .SetConfiguration(Configuration)
+                .SetOutputDirectory(ArtifactsDirectory)
+                .EnableNoBuild()
+                .EnableNoRestore()
+                .When(IsServerBuild, x => x.SetProperty("ContinuousIntegrationBuild", "true"))
+                .SetProject(Solution));
+        });
+
+    Target PushToNuGet => _ => _
+        .DependsOn(Pack)
+        .OnlyWhenStatic(() => IsServerBuild)
+        .Requires(() => NuGetToken)
+        .Executes(() =>
+        {
+            var packages = ArtifactsDirectory.GlobFiles("*.nupkg");
+            DotNetNuGetPush(s => s
+                .SetApiKey(NuGetToken)
+                .SetSource(NugetOrgUrl)
+                .EnableSkipDuplicate()
+                .CombineWith(packages, (x, package) => x
+                    .SetTargetPath(package)));
         });
 }
