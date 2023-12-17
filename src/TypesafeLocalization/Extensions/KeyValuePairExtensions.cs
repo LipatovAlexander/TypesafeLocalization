d824b3f3c8b69e91f389ad277834bc3cbdc7b3eb
LipatovAlexander <lipatov.work@bk.ru>
2023-12-17 23:32:53 +0300

    chore: KeyValuePairExtensions.Deconstruct

---
+++ src/TypesafeLocalization/Extensions/KeyValuePairExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TypesafeLocalization.Extensions;
+
+public static class KeyValuePairExtensions
+{
+    public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value)
+    {
+        key = pair.Key;
+        value = pair.Value;
+    }
+}
